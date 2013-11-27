99dfc43a6d9d0dd130794446e7419892e17b2006
Matt Tucker <matt.tucker@colorado.edu>
2013-11-27 11:28:46 -0700

    Adding example config.

---
+++ example.githubstash.config.php
@@ -0,0 +1,18 @@
+<?php
+
+global $conf;
+
+$conf['github'] = array(
+  'username' => 'my-github-username',
+  'password' => 'my-github-password',
+  'organization' => 'my-github-organization',
+);
+
+$conf['stash'] = array(
+  'username' => 'my-stash-username',
+  'password' => 'my-stash-password',
+  'url' => 'my-stash-url-no-http-no-trailing-slash',
+  'port' => 7999,
+  'project' => 'my-stash-project-name',
+);
+
